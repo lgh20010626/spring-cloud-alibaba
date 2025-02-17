85507ad175de8800aad91f3cfc3b97a55155606f
light <javalgh@163.com>
2025-02-17 22:12:24 +0800

    代码优化

--- food/src/main/java/com/light/food/controller/FoodController.java
+++ food/src/main/java/com/light/food/controller/FoodController.java
@@ -2,7 +2,6 @@ package com.light.food.controller;
 
 import com.light.food.api.AccountControllerAPI;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
